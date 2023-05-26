fe178f1ccc177cc13dd0e7cbe52bf599d560a4dd
Tanner Koza <kozatanner@gmail.com>
2023-05-26 18:14:12 -0500

    Create geoplot

--- navtools/plot.py
+++ navtools/plot.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+import planar
 
 
 def correlation(
@@ -9,7 +13,7 @@ def correlation(
     context: str = "talk",
     label=None,
 ) -> plt.axes:
-    """Plots correlation output from navtools.dsp.parcorr.
+    """Plots a correlation output
 
     Parameters
     ----------
@@ -37,3 +41,93 @@ def correlation(
     plt.ylabel("Correlation Magnitude")
 
     return ax
+
+
+def __compute_zoom_level(latitudes, longitudes):
+    all_pairs = []
+
+    for lon, lat in zip(longitudes, latitudes):
+        all_pairs.append((lon, lat))
+
+    b_box = planar.BoundingBox(all_pairs)
+    if b_box.is_empty:
+        return 0, (0, 0)
+
+    area = b_box.height * b_box.width
+    zoom = np.interp(
+        area,
+        [0, 5**-10, 4**-10, 3**-10, 2**-10, 1**-10, 1**-5],
+        [20, 17, 16, 15, 14, 7, 5],
+    )
+    return zoom, b_box.center
+
+
+def geoplot(
+    df: pd.DataFrame,
+    source: str = None,
+    hover_data: dict = None,
+    labels: dict = None,
+    title: str = "geoplot",
+    color_sequence: px.colors = px.colors.sequential.Rainbow,
+) -> go.Figure:
+    """Plots latitude and longitude trajectories
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame with latitude and longitude labeled as "lat" and "lon", respectively
+    source : str, optional
+        String associated with source of positions in df (eg. sensors, filters, etc.), by default None
+    hover_data : dict, optional
+        True/False key value pairs with df entries, by default None
+    labels : dict, optional
+        Formatted labels for hover_data, by default None
+    color_sequence : px.colors, optional
+        Plotly color sequence, by default px.colors.sequential.Rainbow
+
+
+    Returns
+    -------
+    go.Figure
+        geoplot figure
+    """
+    zoom, center = __compute_zoom_level(df.lat, df.lon)
+
+    fig = px.scatter_mapbox(
+        df,
+        lat="lat",
+        lon="lon",
+        color=source,
+        hover_data=hover_data,
+        labels=labels,
+        zoom=3,
+        color_discrete_sequence=color_sequence,
+        template="seaborn",
+    )
+    fig.update_layout(
+        mapbox_style="white-bg",
+        mapbox_layers=[
+            {
+                "below": "traces",
+                "sourcetype": "raster",
+                "sourceattribution": "United States Geological Survey",
+                "source": [
+                    "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
+                ],
+            }
+        ],
+    )
+    fig.update_layout(
+        title=title,
+        font=dict(
+            family="Courier New, monospace",
+            size=18,
+        ),
+    )
+    fig.update_layout(
+        mapbox=dict(
+            center=go.layout.mapbox.Center(lat=center.y, lon=center.x), zoom=zoom
+        )
+    )
+
+    return fig
