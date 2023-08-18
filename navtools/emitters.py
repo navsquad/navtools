f78e03f21eda011d76d2dcffc743d504331183d8
Tanner Koza <kozatanner@gmail.com>
2023-08-18 18:22:12 -0500

    Jit range and range rate functions

--- navtools/emitters.py
+++ navtools/emitters.py
@@ -7,12 +7,53 @@ from dataclasses import dataclass
 from collections import defaultdict
 from datetime import datetime, timezone
 from skyfield.api import load, EarthSatellite
+from numba import njit
 
 from navtools.exceptions import UnsetReceiverState, UnsupportedConstellation
 
 from laika import AstroDog
 from laika.gps_time import GPSTime
 from laika.helpers import get_el_az
+
+
+def compute_visibility_status(
+    rx_pos: np.array, emitter_pos: np.array, mask_angle: float = 10.0
+):
+    emitter_range, _ = compute_range_and_unit_vector(
+        rx_pos=rx_pos, emitter_pos=emitter_pos
+    )
+    emitter_pos = emitter_pos.reshape(-1, 3)
+
+    rx_lat, rx_lon, rx_alt = pmap.ecef2geodetic(rx_pos[0], rx_pos[1], rx_pos[2])
+    _, _, emitter_down = pmap.ecef2ned(
+        emitter_pos[:, 0],
+        emitter_pos[:, 1],
+        emitter_pos[:, 2],
+        rx_lat,
+        rx_lon,
+        rx_alt,
+    )
+    emitter_el = np.arcsin(-emitter_down / emitter_range)
+    is_visible = np.degrees(emitter_el) >= mask_angle
+
+    return is_visible
+
+
+@njit
+def compute_range_and_unit_vector(rx_pos: np.array, emitter_pos: np.array):
+    rx_pos_rel_sat = rx_pos - emitter_pos
+    range = np.linalg.norm(rx_pos_rel_sat)
+    unit_vector = rx_pos_rel_sat / range
+
+    return range, unit_vector
+
+
+@njit
+def compute_range_rate(rx_vel: np.array, emitter_vel: np.array, unit_vector: np.array):
+    rx_vel_rel_sat = rx_vel - emitter_vel
+    range_rate = np.sum(rx_vel_rel_sat * unit_vector)
+
+    return range_rate
 
 
 @dataclass(frozen=True)
@@ -59,7 +100,7 @@ class SatelliteEmitters:
     @rx_pos.setter
     def rx_pos(self, pos: np.array):
         self._is_rx_state_unset = False
-        self._rx_pos = pos
+        self._rx_pos = np.array(pos)
 
     @rx_vel.setter
     def rx_vel(self, vel: np.array):
@@ -91,8 +132,6 @@ class SatelliteEmitters:
 
     def _compute_los_states(self, emitter_states: dict, is_only_visible_emitters: bool):
         emitters = defaultdict()
-        emitter_prn_log = []
-        emitter_pos_log = []
 
         for emitter_prn, emitter_state in emitter_states.items():
             emitter_pos = emitter_state[0]
@@ -100,13 +139,20 @@ class SatelliteEmitters:
             emitter_clock_bias = emitter_state[2]
             emitter_clock_drift = emitter_state[3]
 
-            emitter_prn_log.append(emitter_prn)
-            emitter_pos_log.append(emitter_pos)
+            # if is_only_visible_emitters:
+            #     is_visible = compute_visibility_status(
+            #         rx_pos=self._rx_pos,
+            #         emitter_pos=emitter_pos,
+            #         mask_angle=self._mask_angle,
+            #     )
 
-            range, unit_vector = self.compute_range_and_unit_vector(
+            #     if not is_visible:
+            #         continue
+
+            range, unit_vector = compute_range_and_unit_vector(
                 rx_pos=self._rx_pos, emitter_pos=emitter_pos
             )
-            range_rate = self.compute_range_rate(
+            range_rate = compute_range_rate(
                 rx_vel=self._rx_vel, emitter_vel=emitter_vel, unit_vector=unit_vector
             )
 
@@ -122,59 +168,7 @@ class SatelliteEmitters:
             )
             emitters[emitter_prn] = emitter_state
 
-        if is_only_visible_emitters:
-            is_visible = self.compute_visibility_status(
-                rx_pos=self._rx_pos,
-                emitter_pos=np.asarray(emitter_pos_log),
-                mask_angle=self._mask_angle,
-            )
-            prns = np.asarray(emitter_prn_log)
-            visible_prns = prns[is_visible]
-            emitters = {prn: emitters[prn] for prn in visible_prns}
-
         return emitters
-
-    @staticmethod
-    def compute_visibility_status(
-        rx_pos: np.array, emitter_pos: np.array, mask_angle: float = 10.0
-    ):
-        emitter_range, _ = SatelliteEmitters.compute_range_and_unit_vector(
-            rx_pos=rx_pos, emitter_pos=emitter_pos
-        )
-        emitter_pos = emitter_pos.reshape(-1, 3)
-
-        rx_lat, rx_lon, rx_alt = pmap.ecef2geodetic(rx_pos[0], rx_pos[1], rx_pos[2])
-        _, _, emitter_down = pmap.ecef2ned(
-            emitter_pos[:, 0],
-            emitter_pos[:, 1],
-            emitter_pos[:, 2],
-            rx_lat,
-            rx_lon,
-            rx_alt,
-        )
-        emitter_el = np.arcsin(-emitter_down / emitter_range)
-        is_visible = np.degrees(emitter_el) >= mask_angle
-
-        return is_visible
-
-    @staticmethod
-    def compute_range_and_unit_vector(rx_pos: np.array, emitter_pos: np.array):
-        rx_pos_rel_sat = rx_pos - emitter_pos
-        range = np.linalg.norm(rx_pos_rel_sat.reshape(-1, 3), axis=1, keepdims=True)
-        unit_vector = rx_pos_rel_sat / range
-
-        return range.squeeze(), unit_vector
-
-    @staticmethod
-    def compute_range_rate(
-        rx_vel: np.array, emitter_vel: np.array, unit_vector: np.array
-    ):
-        rx_vel_rel_sat = rx_vel - emitter_vel
-        range_rate = np.sum(
-            rx_vel_rel_sat.reshape(-1, 3) * unit_vector.reshape(-1, 3), axis=1
-        )
-
-        return range_rate
 
     def _filter_constellations(self, constellations: list):
         if isinstance(constellations, str):
