56483742f914779582d8f6e9ce96ebafe2672d84
Tanner Koza <kozatanner@gmail.com>
2023-11-10 14:22:19 -0600

    Cast real data as complex type

--- navtools/io.py
+++ navtools/io.py
@@ -6,6 +6,7 @@ class SignalFile:  # TODO: correct handling of real-valued data
         self._fid = open(file_path, "rb+")
         self._dtype = np.dtype(dtype)
         self._byte_depth = self._dtype.itemsize
+        self._is_complex = is_complex
 
         MIN_NUMPY_COMPLEX_BYTE_DEPTH = 8
         if is_complex and self._byte_depth < MIN_NUMPY_COMPLEX_BYTE_DEPTH:
@@ -45,5 +46,8 @@ class SignalFile:  # TODO: correct handling of real-valued data
 
         if self._is_complex_with_invalid_dtype:
             samples = samples.astype(np.float32).view(np.complex64)
+
+        if not self._is_complex:
+            samples = samples.astype(np.complex64)
 
         return samples
